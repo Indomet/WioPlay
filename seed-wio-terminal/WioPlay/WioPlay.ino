1fcc638404849945e99a287a87302ce8da6d3b0c
Indomet <mehmetasim35@hotmail.com>
2023-04-09 17:36:25 +0200

    Simplify movement detection and print detection text on device

--- seed-wio-terminal/WioPlay/WioPlay.ino
+++ seed-wio-terminal/WioPlay/WioPlay.ino
@@ -1,7 +1,9 @@
 #include "LIS3DHTR.h"   //include accelerometer library
+#include"TFT_eSPI.h"  // drawing shapes
 
 
 LIS3DHTR <TwoWire> lis;  //Initialize accelerometer
+TFT_eSPI tft;
 
 void setup() {
   Serial.begin(115200);  //Start serial communication
@@ -14,24 +16,37 @@ void setup() {
   }
   lis.setOutputDataRate(LIS3DHTR_DATARATE_25HZ);  //Data output rate (5Hz up to 5kHz)
   lis.setFullScaleRange(LIS3DHTR_RANGE_2G);       //Scale range (2g up to 16g)
+  tft.begin();
+  tft.setRotation(3); // set the screen orientation
+  tft.setTextColor(TFT_BLACK); //set text color
+  tft.setTextSize(3); //set text size 
+
 }
 
 void loop() {
-  float x_values, y_values, z_values;  //Initialize variables to store accelerometer values
+  float current_x, current_y, current_z;  //Initialize variables to store accelerometer values
   float prev_x, prev_y, prev_z;
-  prev_x = lis.getAccelerationX();  //store x-axis accelerometer values
-  prev_y = lis.getAccelerationY();  //store y-axis accelerometer values
-  prev_z = lis.getAccelerationZ();  //store z-axis accelerometer values
-  delay(200);
-  x_values = lis.getAccelerationX();  //store x-axis accelerometer values
-  y_values = lis.getAccelerationY();  //store y-axis accelerometer values
-  z_values = lis.getAccelerationZ();  //store z-axis accelerometer values
 
-  float diff_x = abs(x_values - prev_x);
-  float diff_y = abs(y_values - prev_y);
-  float diff_z = abs(z_values - prev_z);
+  lis.getAcceleration(&prev_x , &prev_y, &prev_z);
+
+  delay(200);
+
+  lis.getAcceleration(&current_x , &current_y, &current_z);
+
+  float diff_x = abs(current_x - prev_x), diff_y = abs(current_y - prev_y), diff_z = abs(current_z - prev_z);
 
   if (diff_x > 0.5 || diff_y > 0.5 || diff_z > 0.5) {  // If the difference is greater than 10 (indicating movement), play the music
     Serial.println("Movement detected!");
+   
+    tft.fillScreen(TFT_BLUE); //fill background 
+    tft.drawString("Movement detected!",0,0); //draw text string 
+    delay(500);
+   
+  } else {
+    Serial.println("No movement");
+    tft.fillScreen(TFT_RED);
+    tft.drawString("No movement!",0,0); 
+    delay(500);
   }
+
 }
