1e8bc7b46f2adaf8c7ce425c4d60f0f8b37f77a5
mohamadk <mohamadk@student.chalmers.se>
2023-04-19 13:48:34 +0200

    add comments and basic splitting

--- NoteParser/src/Core/Parser.java
+++ NoteParser/src/Core/Parser.java
@@ -1,5 +1,10 @@
 package Core;
 
+/*
+This class handles all processing of musical notes to an array of frequencies
+which can then be used in arduino
+*/
+
 public class Parser {
 
     public static String[] splitBars(String content) {
