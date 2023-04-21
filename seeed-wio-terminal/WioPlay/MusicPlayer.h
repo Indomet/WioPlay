260e37f55f91fb523b3c7ebf9cca8c3389c49b97
Chef03 <johnmurphyz166@gmail.com>
2023-04-21 23:51:49 +0200

    add playing logic to music player

--- seeed-wio-terminal/WioPlay/MusicPlayer.h
+++ seeed-wio-terminal/WioPlay/MusicPlayer.h
@@ -20,6 +20,38 @@ public:
     this->position = 0;
   }
 
+private:
+  void bumpPosition() {
+    if (this->position + 5 < this->size) {
+      this->position = this->position + 5;
+    } else {
+      this->position = 0;
+    }
+  }
+
+private:
+  void play(int position) {
+
+    // this is the approximate duration between notes calculated based on the provided tempo
+    int pauseDuration = GENERIC_DURATION * (1 / tempo);
+
+    if (song[position] != 0) {
+      tone(1, song[position], GENERIC_DURATION);
+    }
+
+    delay(pauseDuration);
+    noTone(1);
+  }
+
+public:
+
+  void playChunk() {
+    for (int i = this->position; i < this->position + CHUNK_SIZE; i++) {
+      play(i);
+    }
+    this->bumpPosition();
+  }
+
   int getPosition() {
     return this->position;
   }
