97c237a1f51675655b9494104a224f843ddf7565
Zepei Zhao <84900702+Zepeiz@users.noreply.github.com>
2023-04-22 20:07:21 +0200

    Implement the display of song title, duration, price, unlock status, and temporary placeholder for image, and current user balance.

--- AndroidApp/app/src/main/java/com/example/myapplication/MusicFragment.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/MusicFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 public class MusicFragment extends Fragment {
 
     private RecyclerView recyclerView;
+    private TextView userBalance;
     private View rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,14 +25,20 @@ public class MusicFragment extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_music, container, false);
         recyclerView = rootView.findViewById(R.id.songLibraryView);
+        userBalance = rootView.findViewById(R.id.user_balance);
+
+        userBalance.setText(Integer.toString(MainActivity.user.getCalorieCredit()));
 
 
         //The song list database
         //TODO: replace with reading data from music list file
         ArrayList<Song> songsList = new ArrayList<>();
-        songsList.add(new Song("Song 1", 180, 200, "", true));
+        songsList.add(new Song("Song 1", 185, 200, "", true));
         songsList.add(new Song("Song 2", 200, 250, "", true));
         songsList.add(new Song("Song 3", 170, 300, "", false));
+        songsList.add(new Song("Song 4", 180, 200, "", false));
+        songsList.add(new Song("Song 5", 200, 250, "", false));
+        songsList.add(new Song("Song 6", 173, 300, "", false));
 
         SongLibraryAdapter adapter = new SongLibraryAdapter(recyclerView.getContext());
         adapter.setSongsList(songsList);
--- AndroidApp/app/src/main/java/com/example/myapplication/SongLibraryAdapter.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/SongLibraryAdapter.java
@@ -10,9 +10,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.concurrent.TimeUnit;
 
 public class SongLibraryAdapter extends RecyclerView.Adapter<SongLibraryAdapter.ViewHolder>{
 
@@ -35,15 +39,31 @@ public class SongLibraryAdapter extends RecyclerView.Adapter<SongLibraryAdapter.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         //position is the index of the items in the recycler view
-        holder.textView.setText(songsList.get(position).getTitle());
-        final int currentPosition = position;
+        int currentPosition = holder.getBindingAdapterPosition();
+
+        int duration = songsList.get(currentPosition).getDuration();
+
+
+
+        holder.songTitle.setText(songsList.get(currentPosition).getTitle());
+
+        holder.songDuration.setText((duration / 60) + ":" + String.format("%02d", duration % 60));
+        if(!songsList.get(currentPosition).isUnlocked()){
+            holder.songPrice.setText(Integer.toString(songsList.get(currentPosition).getPrice())+" CC");
+        }else{
+            holder.songPrice.setText("Play");
+        }
+        //TODO: add other attributes
 
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(songsList.get(currentPosition).isUnlocked()){
+                    //TODO: Play song
                     Toast.makeText(context, "Selected " + songsList.get(currentPosition).getTitle(), Toast.LENGTH_SHORT).show();
                 }else{
+                    //TODO: popup window to unlock song by spending Calorie Credits
+
                     Toast.makeText(context, "Locked, " +songsList.get(currentPosition).getTitle() + " costs " + songsList.get(currentPosition).getPrice() + " Calorie Credits", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -62,12 +82,16 @@ public class SongLibraryAdapter extends RecyclerView.Adapter<SongLibraryAdapter.
 
     public class ViewHolder extends RecyclerView.ViewHolder{
     //Holds all views
-        private TextView textView;
-        private RelativeLayout parent;
+        private TextView songTitle, songPrice, songDuration, songImage;
+        private CardView parent;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.song_title);
             parent = itemView.findViewById(R.id.parent_layout);
+            songTitle = itemView.findViewById(R.id.song_title);
+            songPrice = itemView.findViewById(R.id.song_price);
+            songDuration = itemView.findViewById(R.id.song_duration);
+
+
         }
     }
 }
