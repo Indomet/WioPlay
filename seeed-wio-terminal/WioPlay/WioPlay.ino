479a6c354c1102aae583a5e9a72d29c5aaeff7ab
mrjex <joel.mattsson@hotmail.se>
2023-04-27 18:23:46 +0200

    Refactor code into methods without 'magic values'

--- seeed-wio-terminal/WioPlay/WioPlay.ino
+++ seeed-wio-terminal/WioPlay/WioPlay.ino
@@ -7,21 +7,25 @@ UserInformation userInformation (67, 175, 23, 0); // (userWeight, userHeight, us
 DynamicJsonDocument doc(1024);
 #include "MotionDetection.h"
 #include "BurndownChart.h"
-// #include "MqttConnection.h"
+#include "MqttConnection.h"
 #include "MusicPlayer.h"
 
 float movementValue;
 bool isExercising = true;
 
+byte caloriesPerSecondTextCoordinates[2] = {5, 50};
+byte exerciseResultTextCoordinates[2] = {15, 70};
+
 MotionDetection motionDetection;
 BurndownChart burndownChart;
+
 MusicPlayer player(song, sizeof(song) / sizeof(int), 1.2);
 const char* calorie_pub = "Send/Calorie/Burn/Data";
 
 void setup()
 {
   Serial.begin(9600);  //Start serial communication
-  // setupMqtt();
+  setupMqtt();
 
   motionDetection.startAccelerator();
   burndownChart.initializeUI();
@@ -29,7 +33,7 @@ void setup()
 
 void loop()
 {
-  // loopMqtt();
+  loopMqtt();
   
   // Exercise isn't complete yet: Continually read movement-values and update chart accordingly
   if (isExercising)
@@ -53,29 +57,39 @@ void loop()
     movementValue =  motionDetection.detectMotion(); // Read current user-position
     burndownChart.sufficientMovementInquiry(userInformation, movementValue);
     isExercising = burndownChart.isExercising();
-    // client.publish(calorie_pub, String(burndownChartBackEnd.caloriesBurnt).c_str());
+    client.publish(calorie_pub, String(burndownChartBackEnd.getCaloriesBurnt()).c_str());
   }
 
   // Exercise is completed: Inactivate burndown chart and show panel
   else
   {
-    tft.setTextSize(3);
-
-    String caloriesPerSecondComparison = String(burndownChart.displayCalorieStatistics().c_str());
-    tft.drawString(caloriesPerSecondComparison, 5, 50);
-
-    // User completed goal
-    if (burndownChart.checkIfUserAccomplishedGoal())
-    {
-      tft.fillScreen(TFT_GREEN);
-      tft.drawString("Accomplished goal!", 20, 70);
-    }
-
-    // User didn't attain the set goal
-    else
-    {
-      tft.fillScreen(TFT_RED);
-      tft.drawString("Menu here!", 50, 70);
-    }
+    displayExerciseResults();
   }
+}
+
+void displayExerciseResults()
+{
+  tft.setTextSize(3);
+
+  displayCalorieSecondComparison();
+
+  // User completed goal
+  if (burndownChart.checkIfUserAccomplishedGoal())
+  {
+    tft.fillScreen(TFT_GREEN);
+    tft.drawString("Accomplished goal!", exerciseResultTextCoordinates[0], exerciseResultTextCoordinates[1]);
+  }
+
+  // User didn't attain the set goal
+  else
+  {
+    tft.fillScreen(TFT_RED);
+    tft.drawString("Menu here!", exerciseResultTextCoordinates[0], exerciseResultTextCoordinates[1]);
+  }
+}
+
+void displayCalorieSecondComparison()
+{
+  String caloriesPerSecondComparison = String(burndownChart.displayCalorieStatistics().c_str());
+  tft.drawString(caloriesPerSecondComparison, caloriesPerSecondTextCoordinates[0], caloriesPerSecondTextCoordinates[1]);
 }
