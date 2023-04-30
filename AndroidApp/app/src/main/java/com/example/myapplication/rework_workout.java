611e63502733e7979ae3777565ff172d42967919
almuslim <almuslim@net.chalmers.se>
2023-04-30 12:12:55 +0200

    Add layout for the xml file for the ui reword of the settings tab

--- AndroidApp/app/src/main/java/com/example/myapplication/rework_workout.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/rework_workout.java
@@ -1,64 +1,47 @@
 package com.example.myapplication;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link rework_workout#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.harrywhewell.scrolldatepicker.DayScrollDatePicker;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class rework_workout extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View rootView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public rework_workout() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment rework_workout.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static rework_workout newInstance(String param1, String param2) {
-        rework_workout fragment = new rework_workout();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    DayScrollDatePicker weeklyCalendar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rework_workout, container, false);
+        rootView = inflater.inflate(R.layout.fragment_rework_workout, container, false);
+        weeklyCalendar = (DayScrollDatePicker) rootView.findViewById(R.id.day_date_picker);
+
+        weeklyCalendar.getSelectedDate(date -> onDateSelected(date));
+
+        return rootView;
     }
+
+    public void onDateSelected(@Nullable Date date) {
+        if(date != null){
+            // do something with selected date
+        }
+    }
+
 }
