ffac743bc4e93b89bf0526bd73ca82950246e715
Mohamad Khalil <mohamadk@student.chalmers.se>
2023-05-01 16:22:16 +0200

    load song files, and improve MqttConnection design

--- NoteParser/src/main/java/com/wioplay/parser/Main.java
+++ NoteParser/src/main/java/com/wioplay/parser/Main.java
@@ -1,6 +1,5 @@
 package com.wioplay.parser;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
 import com.wioplay.parser.Network.MQTTConnection;
 import com.wioplay.parser.Utils.IOScanner;
 
@@ -8,15 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-
-            new MQTTConnection("Parser");
+            MQTTConnection.getInstance();
             IOScanner.printSeparator();
             System.out.println();
-
-        } catch (MqttException e) {
-            throw new RuntimeException(e);
-        }
 
     }
 
