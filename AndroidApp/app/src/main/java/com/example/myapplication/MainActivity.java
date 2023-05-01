7f23c4963379077354bd745c9b56c98ce5314bf7
jacniy <jacniy@student.chalmers.se>
2023-05-01 08:32:19 +0200

    this commit is for the singelton prinsiple. moddified classes are Main activity,mussicFragment,SettingsFragment, songLibraryAdapter and user

--- AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
 
     //TODO THIS IS TO BE REFACTORED ITS BAD PRACTICE make into singleton pattern
     public static BrokerConnection brokerConnection;
-    public static User user = new User();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
         changeFragment(musicFragment);
         bottomNavigation.setOnItemSelectedListener(this);
         brokerConnection = new BrokerConnection(getApplicationContext());
+        User.getInstance();
 
 
 
