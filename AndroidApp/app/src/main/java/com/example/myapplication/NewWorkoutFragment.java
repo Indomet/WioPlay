d6d9285c0730263b03d17345aec17009bf316e1e
AliMousa27 <jafar.myhero@gmail.com>
2023-05-04 11:45:10 +0200

    Modify pre-exisitng functionality to fit the UI rework of the new workout fragment

--- AndroidApp/app/src/main/java/com/example/myapplication/NewWorkoutFragment.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/NewWorkoutFragment.java
@@ -17,6 +17,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.List;
@@ -25,12 +26,12 @@ import java.util.List;
 public class NewWorkoutFragment extends Fragment {
     private View rootView;
 
-    private EditText durationEditText;
 
     private TextView targetCaloriesTextView;
 
     private WorkoutManager workoutManager;
-    Spinner exerciseTypeSpinner;
+
+    private TimePicker timePicker;
 
     //The following 6 methods are to maintain th lifecycle of the fragment by using the parent class method
     @Override
@@ -66,16 +67,13 @@ public class NewWorkoutFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView= inflater.inflate(R.layout.fragment_new_workout, container, false);
-        exerciseTypeSpinner = rootView.findViewById(R.id.exercise_type_spinner);
 
         List<String> genders = List.of("Running","Walking","Hiking");
         ArrayAdapter<String> exerciseAdapter = new ArrayAdapter<>(rootView.getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,genders);
-        exerciseTypeSpinner.setAdapter(exerciseAdapter);
-        durationEditText = rootView.findViewById(R.id.duration_edit_text);
         Button startWorkoutButton = rootView.findViewById(R.id.start_button);
         startWorkoutButton.setOnClickListener(view->startWorkout());
 
-        targetCaloriesTextView = rootView.findViewById(R.id.user_calorie_goal);
+        targetCaloriesTextView = rootView.findViewById(R.id.current_calorie_goal);
 
         workoutManager = WorkoutManager.getInstance();
 
@@ -86,8 +84,13 @@ public class NewWorkoutFragment extends Fragment {
         incrementButton.setOnClickListener(view -> changeCalorieGoal(workoutManager.getCHANGE_TARGET_CALORIES_AMOUNT()));
         decrementButton.setOnClickListener(view -> changeCalorieGoal(-workoutManager.getCHANGE_TARGET_CALORIES_AMOUNT()));
 
+        timePicker = rootView.findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+        timePicker.setHour(0);
+        timePicker.setMinute(0);
         return rootView;
     }
+
     //here we set the calorie goal based on if the uer want sto burn more or less calories. If they want
     //to decrease the goal, then the parameter passed is negative
     public void changeCalorieGoal(int changeAmount){
@@ -98,28 +101,25 @@ public class NewWorkoutFragment extends Fragment {
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void startWorkout(){
         //validate that time written is valid and the calorie goal are above 0
-        String duration = durationEditText.getText().toString();
-        boolean durationIsValid = workoutManager.validateDuration(duration);
         boolean calorieGoalIsValid = workoutManager.getCalorieGoal()>0;
-
-        if(!durationIsValid){
-            Toast.makeText(rootView.getContext(), "Please input a valid time Hours:Minutes", Toast.LENGTH_SHORT).show(); //Shouldn't this be Minutes:Seconds?
-        }
-        else if(!calorieGoalIsValid){
+        if(!calorieGoalIsValid){
             Toast.makeText(rootView.getContext(), "Please set a calorie goal above 0", Toast.LENGTH_SHORT).show();
         }
 
         else {
             //TODO use singleton
+            //todo fix the workout type thijng
             workoutManager.setWorkoutHasStarted(true);
-            String workoutType = exerciseTypeSpinner.getSelectedItem().toString();
+            //String workoutType = exerciseTypeSpinner.getSelectedItem().toString();
             //publish the exercise type, calorie goal and duration
-            workoutManager.setWorkoutType(workoutType);
-            workoutManager.setDurationInSeconds(workoutManager.timeToSeconds(duration));
-            Log.d("tag","duration in sec is "+workoutManager.getDurationInSeconds());
+            //workoutManager.setWorkoutType(workoutType);
+            final int SECONDS_IN_HOUR = 3600;
+            final int SECONDS_IN_MINUTE = 60;
+            int durationInSeconds = (timePicker.getHour()*SECONDS_IN_HOUR)+(timePicker.getMinute()*SECONDS_IN_MINUTE);
+            workoutManager.setDurationInSeconds(durationInSeconds);
             try {
                 String json = Util.objectToJSON(workoutManager);
-                Toast.makeText(rootView.getContext(), json, Toast.LENGTH_SHORT).show();
+                Toast.makeText(rootView.getContext(), "Your working out for " + durationInSeconds + "secs", Toast.LENGTH_SHORT).show();
                 MainActivity.brokerConnection.getMqttClient().publish(MainActivity.brokerConnection.WORKOUT_STARTED_TOPIC,Util.objectToJSON(workoutManager),MainActivity.brokerConnection.QOS,null);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
