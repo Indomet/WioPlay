965e6f1218d8adc683f5d7ddf31f04381142ea9a
Zepei Zhao <84900702+Zepeiz@users.noreply.github.com>
2023-05-09 16:44:24 +0200

    Add onMessageArrived method for MusicFragment to receive and load all song data sent by server.

--- AndroidApp/app/src/main/java/com/example/myapplication/MusicFragment.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/MusicFragment.java
@@ -6,21 +6,28 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class MusicFragment extends Fragment{
+public class MusicFragment extends Fragment implements BrokerConnection.MessageListener{
 
 
     private RecyclerView recyclerView;
     private TextView userBalance;
     private View rootView;
     private ArrayList<Song> songsList = new ArrayList<>();
-
     private boolean hasCreated = false;
 
     @Override
@@ -33,26 +40,50 @@ public class MusicFragment extends Fragment{
 
         userBalance.setText(Integer.toString(MainActivity.user.getCalorieCredit()));
 
-        if(!hasCreated) {
-            //The song list database
-            //TODO: replace with reading data from music list file
-            songsList.add(new Song("Song 1", 185, 200, "", true));
-            songsList.add(new Song("Song 2", 200, 250, "", true));
-            songsList.add(new Song("Song 3", 170, 300, "", false));
-            songsList.add(new Song("Song 4", 180, 200, "", false));
-            songsList.add(new Song("Song 5", 200, 250, "", false));
-            songsList.add(new Song("Song 6", 173, 300, "", false));
-            hasCreated = true;
-        }
+        MainActivity.brokerConnection = BrokerConnection.getInstance(rootView.getContext());
+        MainActivity.brokerConnection.setMessageListener(this);
+
 
         SongLibraryAdapter adapter = new SongLibraryAdapter(recyclerView.getContext());
         adapter.setSongsList(MainActivity.songList.getSongList());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext(), LinearLayoutManager.VERTICAL, false));
-        //Linear displays a single line of items vertically
-
+        //Linearly displays a single line of items vertically
 
         return rootView;
     }
+
+    @Override
+    public void onMessageArrived(String payload) {
+
+        Toast.makeText(rootView.getContext(), payload, Toast.LENGTH_SHORT).show();
+
+        ArrayList<Song> parsedSongs = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode songs = null;
+        try {
+            songs = mapper.readTree(payload);
+        } catch (IOException e) {
+            Log.d("error", "unable to read payload");
+            throw new RuntimeException(e);
+        }
+        songs.elements().forEachRemaining(node -> {
+
+            String title = node.get("title").asText();
+            String artist = node.get("artist").asText();
+            String imageURL = node.get("imageURL").asText();
+            int cost = node.get("cost").asInt();
+
+            //TODO: include numOfNotes and tempo in payload, currently null
+            double tempo = node.get("tempo").asLong();
+            int numOfNotes = node.get("numOfNotes").asInt();
+            int duration = (int) Math.round(numOfNotes/tempo);
+
+            Song song = new Song(title, artist, duration, cost, imageURL, false);
+            parsedSongs.add(song);
+        });
+        MainActivity.songList.setList(parsedSongs);
+    }
+
 
 }
