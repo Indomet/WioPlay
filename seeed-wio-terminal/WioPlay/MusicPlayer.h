d0caaedf54fd63f8abbcd72b1b49f537c8a05cdf
Chef03 <johnmurphyz166@gmail.com>
2023-05-11 15:19:42 +0200

    Resolve merge conflicts

--- seeed-wio-terminal/WioPlay/MusicPlayer.h
+++ seeed-wio-terminal/WioPlay/MusicPlayer.h
@@ -1,13 +1,13 @@
 #include "pitches.h"
-#include "themes.h"
+// #include "themes.h"
 
-class MusicPlayer {
+class MusicPlayer
+{
 
   static const int GENERIC_DURATION = 1000 / 5;
   static const int CHUNK_SIZE = 5;
 
 private:
-  int* song;
   int size;
   int tempo;
   int position;
@@ -16,29 +16,36 @@ private:
   bool isPaused;
 
 public:
-  MusicPlayer(int* song, int size, float tempo) {
-    this->song = song;
-    this->size = size;
+  DynamicJsonDocument song;
+  MusicPlayer(float tempo) : song(song)
+  {
     this->tempo = tempo;
     this->position = 0;
     this->isPaused = false;
 
     // this is the approximate duration between notes calculated based on the provided tempo
-    pauseDuration = GENERIC_DURATION  / tempo;
+    pauseDuration = GENERIC_DURATION / tempo;
   }
 
 private:
-  void bumpPosition() {
-    if (this->position + 5 < this->size) {
+  void bumpPosition()
+  {
+    if (this->position + 5 < this->size)
+    {
       this->position = this->position + 5;
-    } else {
+    }
+    else
+    {
       this->position = 0;
     }
   }
 
 private:
-  void play(int position) {
-    if (song[position] != 0 && !this->isPaused) {
+  void play(int position)
+  {
+
+    if (song[position] != 0 && !this->isPaused)
+    {
       tone(1, song[position], GENERIC_DURATION);
     }
 
@@ -47,83 +54,90 @@ private:
   }
 
 public:
-  void playChunk() {
-    currentPauseChunkDuration = 0;
+  void playChunk()
+  {
 
-    for (int i = this->position; i < this->position + CHUNK_SIZE; i++) {
+    currentPauseChunkDuration = 0;
+    int limit = min(this->position + CHUNK_SIZE, this->size);
+    for (int i = this->position; i < limit; i++)
+    {
       play(i);
-      registerIncreasedChunkDuration();
+      // registerIncreasedChunkDuration();
     }
     this->bumpPosition();
   }
 
 public:
-  int getPosition() {
+  int getPosition()
+  {
     return this->position;
   }
 
 public:
-  void nextSong() {
-    /*
-    this->position = 0;
-
-    songIdx++;
-
-    if (songIdx == sizeof(allSongs)/sizeof(int))
-    {
-      songIdx = 0;
-    }
-
-    this->song = allSongs[songIdx];
-    */
-  }
-
-public:
-void previousSong() {
-  /*
-  this->position = 0;
-
-  songIdx--;
-
-  if (songIdx < 0)
+  void nextSong()
   {
-    songIdx = sizeof(allSongs)/sizeof(int) - 1;
+    // this->position = 0;
+
+    // songIdx++;
+
+    // if (songIdx == sizeof(allSongs)/sizeof(int))
+    // {
+    //   songIdx = 0;
+    // }
+
+    // this->song = allSongs[songIdx];
   }
 
-  this->song = allSongs[songIdx];
-  */
-}
+public:
+  void previousSong()
+  {
+    // this->position = 0;
+
+    // songIdx--;
+
+    // if (songIdx < 0)
+    // {
+    //   songIdx = sizeof(allSongs)/sizeof(int) - 1;
+    // }
+
+    // this->song = allSongs[songIdx];
+  }
 
 public:
-  void registerIncreasedChunkDuration() {
+  void registerIncreasedChunkDuration()
+  {
     currentPauseChunkDuration += pauseDuration;
   }
 
 public:
-  float getCurrentPauseChunkDuration() {
+  float getCurrentPauseChunkDuration()
+  {
     return currentPauseChunkDuration;
   }
 
 public:
-  void toggle() {
+  void toggle()
+  {
     this->isPaused ? this->resume() : this->pause();
   }
 
 public:
-  void changeSong(int* newSong, int size) {
+  void changeSong(DynamicJsonDocument newSong)
+  {
     this->position = 0;
-    this->size = size;
+    this->size = newSong.size();
     this->song = newSong;
   }
 
 private:
-  void pause() {
+  void pause()
+  {
     this->isPaused = true;
   }
 
 private:
-void resume(){
-  this->isPaused = false;
-}
-
+  void resume()
+  {
+    this->isPaused = false;
+  }
 };
