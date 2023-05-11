cc2e039f39c054b5f2cd0ace495b95a4f5de607d
Mohamad Khalil <mohamadk@student.chalmers.se>
2023-05-11 15:19:42 +0200

    works for non ram heavy notes

--- seeed-wio-terminal/WioPlay/Scenes.h
+++ seeed-wio-terminal/WioPlay/Scenes.h
@@ -1,35 +1,35 @@
 TFT_eSPI tft;
 
 #include "TFT_eSPI.h"
-#include "RawImage.h"  // image processing library
+#include "RawImage.h" // image processing library
 
-
-#define RIGHT_BUTTON WIO_5S_RIGHT  // Right joystick  WIO_5S_PRESS press is probably using same pins as the burndown chart
+#define RIGHT_BUTTON WIO_5S_RIGHT // Right joystick  WIO_5S_PRESS press is probably using same pins as the burndown chart
 #define BUTTON_NEXT WIO_KEY_A
 #define BUTTON_PAUSE WIO_KEY_B
 #define BUTTON_PREVIOUS WIO_KEY_C
 
 bool isOnMusicScene = true;
 bool messageReceived = false;
-const char* artwork = "/photos/ali.bmp";
-const char* prevButton = "/photos/prev.bmp";
-const char* pauseButton = "/photos/pause.bmp";
-const char* nextButton = "/photos/next.bmp";
+const char *artwork = "/photos/ali.bmp";
+const char *prevButton = "/photos/prev.bmp";
+const char *pauseButton = "/photos/pause.bmp";
+const char *nextButton = "/photos/next.bmp";
 String songName;
 
-void updateSongName(const char* newSongName) {
+void updateSongName(const char *newSongName)
+{
   songName = newSongName;
   messageReceived = true;
-
-
 }
 
-void showPlayerScene() {
+void showPlayerScene()
+{
 
-  if (isOnMusicScene) {
+  if (isOnMusicScene)
+  {
 
-    tft.fillScreen(TFT_WHITE);    // declare a backgroundColor
-    tft.setTextColor(TFT_BLACK);  // initializee the text color to white
+    tft.fillScreen(TFT_WHITE);   // declare a backgroundColor
+    tft.setTextColor(TFT_BLACK); // initializee the text color to white
     tft.setTextSize(2);
     // songName = "Song Name here";
     drawImage<uint16_t>(artwork, 95, 25);
@@ -37,9 +37,9 @@ void showPlayerScene() {
     // Update TFT display and print input message
     // tft.setTextColor(textColor, bgColor);  // set the text and background color
 
-    tft.setCursor((320 - tft.textWidth(songName)) / 2, 165);  // Make sure to align the text to the center of the screen
-    tft.println(songName);                                    // print the text
-   // display the buttons
+    tft.setCursor((320 - tft.textWidth(songName)) / 2, 165); // Make sure to align the text to the center of the screen
+    tft.println(songName);                                   // print the text
+                                                             // display the buttons
     drawImage<uint16_t>(prevButton, 100, 200);
     drawImage<uint16_t>(pauseButton, 150, 200);
     drawImage<uint16_t>(nextButton, 200, 200);
@@ -47,20 +47,22 @@ void showPlayerScene() {
     isOnMusicScene = false;
   }
 
-  if (messageReceived) {
+  if (messageReceived)
+  {
     // this is to fill the previous song name with white so that we dont write it on top of the previous text
     tft.fillRect(0, 165, tft.width(), tft.fontHeight() * 2, TFT_WHITE);
-    tft.setCursor((320 - tft.textWidth(songName)) / 2, 165);  // Make sure to align the text to the center of the screen
-    tft.println(songName);                                 // display the new song name
+    tft.setCursor((320 - tft.textWidth(songName)) / 2, 165); // Make sure to align the text to the center of the screen
+    tft.println(songName);                                   // display the new song name
     messageReceived = false;
   }
 }
 
-
-void setupButton() {
+void setupButton()
+{
   pinMode(RIGHT_BUTTON, INPUT_PULLUP);
 
-  while (digitalRead(RIGHT_BUTTON) == LOW) {
+  while (digitalRead(RIGHT_BUTTON) == LOW)
+  {
     // Wait for button to be released
   }
 
@@ -69,11 +71,14 @@ void setupButton() {
   pinMode(BUTTON_PREVIOUS, INPUT);
 }
 
-void buttonOnPress() {
+void buttonOnPress()
+{
   // Check if button A is pressed
-  if (digitalRead(BUTTON_NEXT) == LOW) {
+  if (digitalRead(BUTTON_NEXT) == LOW)
+  {
     // Wait for button to be released
-    do {
+    do
+    {
       // delay(10);  // Wait for a short time
     } while (digitalRead(BUTTON_NEXT) == LOW);
 
@@ -81,9 +86,11 @@ void buttonOnPress() {
   }
 
   // Check if button B is pressed
-  if (digitalRead(BUTTON_PAUSE) == LOW) {
+  if (digitalRead(BUTTON_PAUSE) == LOW)
+  {
     // Wait for button to be released
-    do {
+    do
+    {
       // delay(10);  // Wait for a short time
     } while (digitalRead(BUTTON_PAUSE) == LOW);
 
@@ -92,24 +99,29 @@ void buttonOnPress() {
   }
 
   // Check if button C is pressed
-  if (digitalRead(BUTTON_PREVIOUS) == LOW) {
+  if (digitalRead(BUTTON_PREVIOUS) == LOW)
+  {
     // Wait for button to be released
-    do {
+    do
+    {
       // delay(10);  // Wait for a short time
     } while (digitalRead(BUTTON_PREVIOUS) == LOW);
 
-    //play music when button C is pressed
+    // play music when button C is pressed
     player.previousSong();
   }
 }
- 
 
-void menuNavigationOnPress(void (*firstScene)(), void (*secondScene)()) {
-  if (digitalRead(RIGHT_BUTTON) == LOW) {
+void menuNavigationOnPress(void (*firstScene)(), void (*secondScene)())
+{
+  if (digitalRead(RIGHT_BUTTON) == LOW)
+  {
     // Button is held down
     secondScene();
-    isOnMusicScene = true;  // update the music player sceen only when the button is released
-  } else {
+    isOnMusicScene = true; // update the music player sceen only when the button is released
+  }
+  else
+  {
     // Button is not held down
     firstScene();
   }
