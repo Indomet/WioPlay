b2a843a9f4dda9a1f0a0b7c1d5c5509451bfec8f
almuslim <almuslim@net.chalmers.se>
2023-05-15 13:18:48 +0200

    Add a method to util to find the workout manager file for modularity purposes

--- AndroidApp/app/src/main/java/com/example/myapplication/Util.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/Util.java
@@ -1,12 +1,19 @@
 package com.example.myapplication;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.app.Activity;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.File;
 import java.io.IOException;
 
 
 public class Util {
 
+    public static final String WORKOUT_MANAGER_PATH = "/workoutManager.json";
     public static String objectToJSON(Object object) throws IllegalArgumentException, IllegalAccessException {
         ObjectWriter jsonWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String json = null;
@@ -16,6 +23,12 @@ public class Util {
             e.printStackTrace();
         }
         return json;
+    }
+
+    public static WorkoutManager loadManagerFromFile(Activity activity){
+        String managerPath = activity.getFilesDir().getPath() + Util.WORKOUT_MANAGER_PATH;
+        File managerFile = new File(managerPath);
+        return WorkoutManager.getInstance(managerFile);
     }
 
 }
