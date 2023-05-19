337143331a059b8224a99f392fb7caf6eb0e4f2d
Zepei Zhao <84900702+Zepeiz@users.noreply.github.com>
2023-05-19 13:15:38 +0200

    Implement function to save list of unlocked songs and be able to cycle through the list using the forward/back buttons. Co-authored-by: joelmat <joelmat@student.chalmers.se>

--- AndroidApp/app/src/main/java/com/example/myapplication/BrokerConnection.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/BrokerConnection.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import info.mqtt.android.service.Ack;
-//import jdk.internal.org.jline.utils.Log;
 
 public class BrokerConnection extends AppCompatActivity {
 
@@ -24,16 +23,16 @@ public class BrokerConnection extends AppCompatActivity {
 
     public static final String SONG_BUTTON_TOPIC = "Music/Song/Buttons";
     public static final String SONG_NOTES_TOPIC = "Music/Song/Notes";
-    public static final String LOCALHOST = "192.168.227.80" ; // Ip address of the local host
+    public static final String LOCALHOST = "broker.hivemq.com"; // Ip address of the local host
     private static final String MQTT_SERVER = "tcp://" + LOCALHOST + ":1883";   // the server uses tcp protocol on the local host ip and listens to the port 1883
-    public static final String CLIENT_ID = "Androidghfkhg Phone";   // the app client ID name
+    public static final String CLIENT_ID = "a1w2exr@$23xjdq%w1jex";   // the app client ID name
     public static final int QOS = 0;    // quality of service
 
     private boolean isConnected = false;
     private MqttClient mqttClient;
     private Context context;
 
-    private static BrokerConnection instance; // this is for singelton for this class.
+    private static BrokerConnection instance; // this is for singleton for this class.
 
     //Alternatively this could be an arraylist to notify many subscribers to the message listeners
     //private MessageListener messageListener;
@@ -78,9 +77,10 @@ public class BrokerConnection extends AppCompatActivity {
 
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    //if we suceeded in connecting then we subscribe to the topic
+                    //if we succeeded in connecting then we subscribe to the topic
                     isConnected = true;
                     final String successfulConnection = "Connected to MQTT broker";
+                    Log.d(CLIENT_ID, successfulConnection);
                     Toast.makeText(context, successfulConnection, Toast.LENGTH_LONG).show();
                     for(MessageListener listener : observers){
                         String topic = listener.getSubbedTopic();
@@ -115,7 +115,7 @@ public class BrokerConnection extends AppCompatActivity {
 
                 @Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    
+
                     //check if the topic of the message is the subscribed topic
                     if(isConnected){ //TODO: Make separate messageListeners for each fragment
                         //Toast.makeText(context, "The message is "+ message.toString(), Toast.LENGTH_SHORT).show();
