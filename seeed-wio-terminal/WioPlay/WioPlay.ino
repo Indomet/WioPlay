c3e2d9507ce029b5f246650f44a4f7a2d30479a5
mehmetas <mehmetas@student.chalmers.se>
2023-05-19 22:01:20 +0200

    refactor the merged cod to make it compilable

--- seeed-wio-terminal/WioPlay/WioPlay.ino
+++ seeed-wio-terminal/WioPlay/WioPlay.ino
@@ -3,25 +3,24 @@
 #include <ArduinoJson.h>  // json library
 #include "Seeed_FS.h"     // SD card library
 #include "UserInformation.h"
-
 UserInformation userInformation(67, 175, 23, 0);  // (userWeight, userHeight, userAge, isMale)
 
 #include "MotionDetection.h"
 #include "MusicPlayer.h"
+
 
 MusicPlayer player(2);
 
 #include "Scenes.h"
 Scenes scenes;
 #include "BurndownChart.h"
-
-float movementValue;
-
 MotionDetection motionDetection;
 BurndownChart burndownChart;
-#include "MqttConnection.h"
 
-int tempCounter = 0;
+#include "MqttConnection.h"
+#include "ButtonHandler.h"
+ButtonHandler button;
+float movementValue;
 
 void setup() {
   Serial.begin(9600);  // Start serial communication
@@ -33,25 +32,23 @@ void setup() {
   }
 
   motionDetection.startAccelerator();
-  burndownChart.initializeUI();
-
-  // burndownChart.updateGraphVizuals();  // menuNavigationOnPress(showBurndownChartScene, showPlayerScene); //this is here to start burndownchart in the background
+  burndownChart.initializeUI(); //this is here to start burndownchart in the background
+  
+  burndownChart.updateGraphVizuals(); //this is here to start burndownchart in the background
 }
 
 void loop() {
   loopMqtt();
 
   // Exercise isn't complete yet: Continually read movement-values and update chart accordingly
-  if (burndownChart.isExercising()) { // burndownChart.isExercising()
+  if (burndownChart.isExercising()) {
 
     burndownChart.controlConstraints();
     button.onPress();
     button.menuNavigationOnPress(showPlayerScene, showBurndownChartScene);
 
     motionDetection.recordPreviousAcceleration();  // Read previous user-position
-    bool isPlayingSong = player.isPlayingSong();
-
-
+    
     if (player.song.size() > 0 && !player.hasRequested) {
       if (player.getPosition() >= player.song.size()) {
         Serial.println(player.getPosition());
@@ -61,27 +58,14 @@ void loop() {
         player.playChunk();
       }
     } else {
-      delay(100);
+      delay(burndownChart.getUpdateDelay());
     }
 
-
-    float updateDelay = 100;
-    // burndownChart.updateTimeElapsed(1000); // player.getCurrentPauseChunkDuration()
-
-    burndownChart.updateTimeElapsed(updateDelay);
-
-
-    // Note: Commit 'future updates' statistics
-    // Serial.println("***********************");
-    // Serial.println(burndownChart.getTimeElapsed());
-    // Serial.println(burndownChart.getActualCaloriesPerSecond());
-    // Serial.println(burndownChart.getExpectedCaloriesPerSecond());
-    // Serial.println("***********************");
+    burndownChart.updateTimeElapsed();
 
     movementValue = motionDetection.detectMotion();  // Read current user-position
-
-    // burndownChart.sufficientMovementInquiry(userInformation, movementValue, 1000); // player.getCurrentPauseChunkDuration() -------------> Add if-statement for this case
-    burndownChart.sufficientMovementInquiry(userInformation, movementValue, updateDelay);
+    
+    burndownChart.sufficientMovementInquiry(userInformation, movementValue);
 
     client.publish(calorie_pub, String(burndownChartBackEnd.getCaloriesBurnt()).c_str());
   }
